b825e3995812ed89b0702481927a34a58cbf4d9d
Sheema Syed <shma.syd975@gmail.com>
2025-08-11 22:35:35 +0530

    Updated

--- src/components/MiraculousBox/MiraculousBoxCanonical.tsx
+++ src/components/MiraculousBox/MiraculousBoxCanonical.tsx
@@ -90,74 +90,43 @@ const MiraculousBoxCanonical: React.FC<MiraculousBoxCanonicalProps> = ({
 
   const displayCharacter = getSelectedCharacter();
 
-  // Render the canonical miraculous box
+  // Render the new beautiful circular miraculous box
+  const miraculousOrder = [
+    'ladybug', 'cat', 'fox', 'bee', 'peacock', 'butterfly', 'turtle',
+    'rabbit', 'dragon', 'horse', 'monkey', 'rooster', 'dog', 'pig',
+    'ox', 'tiger', 'mouse', 'goat', 'snake'
+  ];
+
   const renderCanonicalBox = () => (
     <div className="miraculous-box-replica">
-      {/* Center Circle - Ladybug & Cat Noir */}
-      <div className="center-circle">
-        <div className="center-jewel-ladybug" onClick={() => handleMiraculousClick('ladybug')}>
+      <div className="miraculous-jewel-circle">
+        {/* Place Ladybug in the center */}
+        <div
+          className="miraculous-jewel center"
+          onClick={() => handleMiraculousClick('ladybug')}
+        >
           <img src={getMiraculousImage('ladybug')} alt="Ladybug Miraculous" />
         </div>
-        <div className="center-jewel-cat" onClick={() => handleMiraculousClick('cat')}>
-          <img src={getMiraculousImage('cat')} alt="Cat Miraculous" />
-        </div>
-      </div>
-
-      {/* Inner Pentagon - 5 Main Miraculous */}
-      <div className="inner-hexagon">
-        <div className="inner-jewel inner-top" onClick={() => handleMiraculousClick('fox')}>
-          <img src={getMiraculousImage('fox')} alt="Fox Miraculous" />
-        </div>
-        <div className="inner-jewel inner-top-right" onClick={() => handleMiraculousClick('bee')}>
-          <img src={getMiraculousImage('bee')} alt="Bee Miraculous" />
-        </div>
-        <div className="inner-jewel inner-bottom-right" onClick={() => handleMiraculousClick('peacock')}>
-          <img src={getMiraculousImage('peacock')} alt="Peacock Miraculous" />
-        </div>
-        <div className="inner-jewel inner-bottom" onClick={() => handleMiraculousClick('butterfly')}>
-          <img src={getMiraculousImage('butterfly')} alt="Butterfly Miraculous" />
-        </div>
-        <div className="inner-jewel inner-left" onClick={() => handleMiraculousClick('turtle')}>
-          <img src={getMiraculousImage('turtle')} alt="Turtle Miraculous" />
-        </div>
-      </div>
-
-      {/* Outer Ring - 12 Rectangular Compartments */}
-      <div className="outer-jewel compartment-top-left" onClick={() => handleMiraculousClick('rabbit')}>
-        <img src={getMiraculousImage('rabbit')} alt="Rabbit Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-top-center" onClick={() => handleMiraculousClick('dragon')}>
-        <img src={getMiraculousImage('dragon')} alt="Dragon Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-top-right" onClick={() => handleMiraculousClick('horse')}>
-        <img src={getMiraculousImage('horse')} alt="Horse Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-right-top" onClick={() => handleMiraculousClick('monkey')}>
-        <img src={getMiraculousImage('monkey')} alt="Monkey Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-right-bottom" onClick={() => handleMiraculousClick('rooster')}>
-        <img src={getMiraculousImage('rooster')} alt="Rooster Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-bottom-right" onClick={() => handleMiraculousClick('dog')}>
-        <img src={getMiraculousImage('dog')} alt="Dog Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-bottom-center" onClick={() => handleMiraculousClick('pig')}>
-        <img src={getMiraculousImage('pig')} alt="Pig Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-bottom-left" onClick={() => handleMiraculousClick('ox')}>
-        <img src={getMiraculousImage('ox')} alt="Ox Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-left-bottom" onClick={() => handleMiraculousClick('tiger')}>
-        <img src={getMiraculousImage('tiger')} alt="Tiger Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-left-center" onClick={() => handleMiraculousClick('mouse')}>
-        <img src={getMiraculousImage('mouse')} alt="Mouse Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-left-top" onClick={() => handleMiraculousClick('goat')}>
-        <img src={getMiraculousImage('goat')} alt="Goat Miraculous" />
-      </div>
-      <div className="outer-jewel compartment-snake" onClick={() => handleMiraculousClick('snake')}>
-        <img src={getMiraculousImage('snake')} alt="Snake Miraculous" />
+        {/* Place the other 18 miraculous evenly around the circle */}
+        {miraculousOrder.filter(m => m !== 'ladybug').map((miraculousId, idx) => {
+          // 18 jewels around the circle
+          const angle = (idx * (360 / 18));
+          return (
+            <div
+              key={miraculousId}
+              className="miraculous-jewel"
+              style={{
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                transform: `translate(-50%, -50%) rotate(${angle}deg) translate(180px) rotate(-${angle}deg)`
+              }}
+              onClick={() => handleMiraculousClick(miraculousId)}
+            >
+              <img src={getMiraculousImage(miraculousId)} alt={`${miraculousId.charAt(0).toUpperCase() + miraculousId.slice(1)} Miraculous`} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
